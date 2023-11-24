63e6750589d640ec6be5e793ccdad2de623b483a
vitor-ufms <vitor-ufms>
2023-11-24 12:58:12 -0400

    new bird

--- source/Controller/test_scap.py
+++
@@ -1,8 +0,0 @@
-from scapy.all import IP, ICMP
-
-# Crie um pacote com cabeçalho IP seguido por um cabeçalho ICMP
-packet = IP(src="192.168.1.1", dst="8.8.8.8") / ICMP()
-print('a')
-# Exiba apenas o cabeçalho IP
-ip_header = packet[IP]
-print(ip_header.show())
